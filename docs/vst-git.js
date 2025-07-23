48391e59260e2036fb2e62ed7db9c1e6c13e5653
Vladimir Lysov <dubaua@gmail.com>
2025-07-23 15:50:29 +0500

    add docs

---
+++ docs/vst-git.js
@@ -0,0 +1,2 @@
+function e(){let e=document.querySelector(".header");0===(window.scrollY||document.documentElement.scrollTop)?e.classList.add("header__transparent"):e.classList.remove("header__transparent")}window.addEventListener("scroll",e),window.addEventListener("resize",e),e();
+//# sourceMappingURL=vst-git.js.map
